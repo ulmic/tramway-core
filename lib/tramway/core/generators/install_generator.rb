43bb0672afdc17a4b752a678d08003cb8ed38d94
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-02 17:03:23 +0400

    Update generator

--- lib/tramway/core/generators/install_generator.rb
+++ lib/tramway/core/generators/install_generator.rb
@@ -4,6 +4,11 @@ module Tramway::Core::Generators
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
+    def self.next_migration_number(path)
+      next_migration_number = current_migration_number(path) + 1
+      ActiveRecord::Migration.next_migration_number next_migration_number
+    end
+
     def copy_initializer
       copy_file "/#{File.dirname __dir__}/generators/templates/initializers/simple_form.rb", 'config/initializers/simple_form.rb'
       copy_file "/#{File.dirname __dir__}/generators/templates/initializers/simple_form_bootstrap.rb", 'config/initializers/simple_form_bootstrap.rb'
