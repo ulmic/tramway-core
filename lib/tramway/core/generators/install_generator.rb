9157d817db3ad3ba0da19ccaf49de7fbe7ddde74
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-02 17:09:30 +0400

    Fix install_generator

--- lib/tramway/core/generators/install_generator.rb
+++ lib/tramway/core/generators/install_generator.rb
@@ -1,17 +1,21 @@
 require 'rails/generators'
 
-module Tramway::Core::Generators
-  class InstallGenerator < ::Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
+module Tramway
+  module Core
+    module Generators
+      class InstallGenerator < ::Rails::Generators::Base
+        source_root File.expand_path('../templates', __FILE__)
 
-    def self.next_migration_number(path)
-      next_migration_number = current_migration_number(path) + 1
-      ActiveRecord::Migration.next_migration_number next_migration_number
-    end
+        def self.next_migration_number(path)
+          next_migration_number = current_migration_number(path) + 1
+          ActiveRecord::Migration.next_migration_number next_migration_number
+        end
 
-    def copy_initializer
-      copy_file "/#{File.dirname __dir__}/generators/templates/initializers/simple_form.rb", 'config/initializers/simple_form.rb'
-      copy_file "/#{File.dirname __dir__}/generators/templates/initializers/simple_form_bootstrap.rb", 'config/initializers/simple_form_bootstrap.rb'
+        def copy_initializer
+          copy_file "/#{File.dirname __dir__}/generators/templates/initializers/simple_form.rb", 'config/initializers/simple_form.rb'
+          copy_file "/#{File.dirname __dir__}/generators/templates/initializers/simple_form_bootstrap.rb", 'config/initializers/simple_form_bootstrap.rb'
+        end
+      end
     end
   end
 end
